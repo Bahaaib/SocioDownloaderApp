9fab51e63c1a283a5ffc6a660038c05e6c33abd9
Bahaaib <bahaasco@gmail.com>
2019-10-19 23:12:40 +0200

    added downloads list

--- app/src/main/java/com/bahaa/sociodownloader/Models/VideoModel.java
+++ app/src/main/java/com/bahaa/sociodownloader/Models/VideoModel.java
@@ -1,4 +1,47 @@
 package com.bahaa.sociodownloader.Models;
 
 public class VideoModel {
+
+    private String name;
+
+    private String path;
+
+    private String size;
+
+    private String date;
+
+    public VideoModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
